78981912e60729b1e4c2c30fa643da2d5f1eca59
Joe Huss <detain@interserver.net>
2017-07-19 10:05:10 -0400

    test updates

--- tests/ParallelsTest.php
+++ tests/ParallelsTest.php
@@ -8,205 +8,204 @@ use PHPUnit\Framework\TestCase;
  */
 class ParallelsTest extends TestCase
 {
-    /**
-     * @var Parallels
-     */
-    protected $object;
+	/**
+	 * @var Parallels
+	 */
+	protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->object = new Parallels;
-    }
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp()
+	{
+		$this->object = new Parallels;
+	}
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown()
+	{
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::authInfo
-     * @todo   Implement testauthInfo().
-     */
-    public function testauthInfo()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::authInfo
+	 */
+	public function testauthInfo()
+	{
+		$auth = $this->object->authInfo();
+		$this->assertTrue(is_array($auth), 'returned proper type');
+		$this->assertTrue(array_key_exists('login', $auth), 'returned required fields');
+		$this->assertTrue(array_key_exists('password', $auth), 'returned required fields');
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::serverAddress
-     * @todo   Implement testserverAddress().
-     */
-    public function testserverAddress()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::serverAddress
+	 * @todo   Implement testserverAddress().
+	 */
+	public function testserverAddress()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::terminateKey
-     * @todo   Implement testTerminateKey().
-     */
-    public function testTerminateKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::terminateKey
+	 * @todo   Implement testTerminateKey().
+	 */
+	public function testTerminateKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::resetKey
-     * @todo   Implement testResetKey().
-     */
-    public function testResetKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::resetKey
+	 * @todo   Implement testResetKey().
+	 */
+	public function testResetKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::activateKey
-     * @todo   Implement testActivateKey().
-     */
-    public function testActivateKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::activateKey
+	 * @todo   Implement testActivateKey().
+	 */
+	public function testActivateKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::addNoteToKey
-     * @todo   Implement testAddNoteToKey().
-     */
-    public function testAddNoteToKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::addNoteToKey
+	 * @todo   Implement testAddNoteToKey().
+	 */
+	public function testAddNoteToKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::sendKeyByEmail
-     * @todo   Implement testSendKeyByEmail().
-     */
-    public function testSendKeyByEmail()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::sendKeyByEmail
+	 * @todo   Implement testSendKeyByEmail().
+	 */
+	public function testSendKeyByEmail()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::createKey
-     * @todo   Implement testCreateKey().
-     */
-    public function testCreateKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::createKey
+	 * @todo   Implement testCreateKey().
+	 */
+	public function testCreateKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::retrieveKey
-     * @todo   Implement testRetrieveKey().
-     */
-    public function testRetrieveKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::retrieveKey
+	 * @todo   Implement testRetrieveKey().
+	 */
+	public function testRetrieveKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::getAvailableUpgrades
-     * @todo   Implement testGetAvailableUpgrades().
-     */
-    public function testGetAvailableUpgrades()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::getAvailableUpgrades
+	 * @todo   Implement testGetAvailableUpgrades().
+	 */
+	public function testGetAvailableUpgrades()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::getKeyInfo
-     * @todo   Implement testGetKeyInfo().
-     */
-    public function testGetKeyInfo()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::getKeyInfo
+	 * @todo   Implement testGetKeyInfo().
+	 */
+	public function testGetKeyInfo()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::getMainKeyFromIp
-     * @todo   Implement testGetMainKeyFromIp().
-     */
-    public function testGetMainKeyFromIp()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::getMainKeyFromIp
+	 * @todo   Implement testGetMainKeyFromIp().
+	 */
+	public function testGetMainKeyFromIp()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::getKeysInfoByIP
-     * @todo   Implement testGetKeysInfoByIP().
-     */
-    public function testGetKeysInfoByIP()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::getKeysInfoByIP
+	 * @todo   Implement testGetKeysInfoByIP().
+	 */
+	public function testGetKeysInfoByIP()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::getKeyNumbers
-     * @todo   Implement testGetKeyNumbers().
-     */
-    public function testGetKeyNumbers()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::getKeyNumbers
+	 * @todo   Implement testGetKeyNumbers().
+	 */
+	public function testGetKeyNumbers()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Parallels\Parallels::getAvailableKeyTypesAndFeatures
-     * @todo   Implement testGetAvailableKeyTypesAndFeatures().
-     */
-    public function testGetAvailableKeyTypesAndFeatures()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Parallels\Parallels::getAvailableKeyTypesAndFeatures
+	 * @todo   Implement testGetAvailableKeyTypesAndFeatures().
+	 */
+	public function testGetAvailableKeyTypesAndFeatures()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 }
